f10e17ffef02d809fc2e5a4eb7c6f56e63e4b0e2
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-09-07 16:58:42 -0300

    refactor: changes server port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,11 @@ import routes from "./routes";
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.use(cors())
 app.use(routes)
 
-app.listen(3333)
+app.listen(port, () => {
+  console.info('Aplicação rodando em https://localhost:3000')
+})
