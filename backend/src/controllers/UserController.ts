d0a9c9d5abefe64649dbcac4a23e3693cc0a1a08
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-09-08 22:13:23 -0300

    refactor: changes an user on db data

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 
 const users = [
-  { id: 0, name: 'Ada Lovelace', age: 35, gender: 'F' },
+  { id: 0, name: 'Avril Lavigne', age: 37, gender: 'F' },
   { id: 1, name: 'Chris Martin', age: 45, gender: 'm' },
   { id: 2, name: 'Letícia Oliveira', age: 28, gender: 'F' },
   { id: 3, name: 'Marcos Paulo', age: 26, gender: 'M' },
