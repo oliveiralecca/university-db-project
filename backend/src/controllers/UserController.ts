abc4138d4aac32a7b22774ca8cefd105e8c2c1b7
oliveiralecca <oliveiraslc@yahoo.com.br>
2022-09-08 22:09:26 -0300

    fix: db data alphabetic order

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -5,8 +5,8 @@ const users = [
   { id: 1, name: 'Chris Martin', age: 45, gender: 'm' },
   { id: 2, name: 'Letícia Oliveira', age: 28, gender: 'F' },
   { id: 3, name: 'Marcos Paulo', age: 26, gender: 'M' },
-  { id: 4, name: 'Rodrigo Santana', age: 20, gender: 'm' },
   { id: 5, name: 'Rihanna', age: 34, gender: 'f' },
+  { id: 4, name: 'Rodrigo Santana', age: 20, gender: 'm' },
   { id: 6, name: 'Slash', age: 57, gender: 'M' },
 ]
 
